aaec445df6b270a6769e019a4a3c4fc7d851aa15
Aman Raj <amannitp272@gmail.com>
2021-02-27 10:52:28 +0530

    detect a loop in LinkedList

---
+++ GeekforGeeks/LinkedList/Q7.java
@@ -0,0 +1,15 @@
+public class Q7 {
+        public static boolean detectLoop(Node head){
+            Node temp=head;
+            Node temp2=head;
+            while(temp!=null && temp2!=null && temp2.next!=null){
+                temp=temp.next;
+                temp2=temp2.next.next;
+                if(temp==temp2)
+                return true;
+                
+             }
+             return false;
+        }
+    }
+    
