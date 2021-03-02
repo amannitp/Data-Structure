2763784ac0dde458fd9683f0a06b93dab3f6a420
Aman Raj <amannitp272@gmail.com>
2021-03-02 15:41:13 +0530

    Finding middle element in a linked list

---
+++ GeekforGeeks/LinkedList/Q19.java
@@ -0,0 +1,21 @@
+public class Q19 {
+    
+    int getMiddle(Node head)
+    
+    {
+        int count=0;
+        Node temp=head;
+        Node temp2=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        int check=count/2;
+       for(int i=1;temp2!=null && i<count;temp2=temp2.next,i++){
+           if(i==(check+1))
+           return temp2.data;
+       }
+       return temp2.data;
+         // Your code here.
+    }
+}
